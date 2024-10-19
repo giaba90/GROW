4d7052fd1009e3086f112e176823d8cbc2ef722c
Gianluca <giaba90@gmail.com>
2024-10-19 18:20:12 +0200

    add client.ts file for graphql

--- client/src/components/App.tsx
+++ client/src/components/App.tsx
@@ -1,25 +1,18 @@
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
-import { ApolloClient, InMemoryCache, ApolloProvider } from '@apollo/client';
 import Homepage from '../pages/homepage';
 import Archive from '../pages/archive';
 import SinglePost from '../pages/single-post';
 
-const client = new ApolloClient({
-  uri: 'http://localhost:8000/graphql', // Sostituisci con l'URL del tuo endpoint GraphQL di WordPress
-  cache: new InMemoryCache(),
-});
 
 function App() {
   return (
-    <ApolloProvider client={client}>
-      <Router>
-        <Routes>
-          <Route path="/" element={<Homepage />} />
-          <Route path="/archive" element={<Archive />} />
-          <Route path="/post/:id" element={<SinglePost />} />
-        </Routes>
-      </Router>
-    </ApolloProvider>
+    <Router>
+      <Routes>
+        <Route path="/" element={<Homepage />} />
+        <Route path="/archive" element={<Archive />} />
+        <Route path="/post/:id" element={<SinglePost />} />
+      </Routes>
+    </Router>
   );
 }
 
--- client/src/main.tsx
+++ client/src/main.tsx
@@ -1,10 +1,13 @@
-import { StrictMode } from 'react'
+import React from 'react'
 import { createRoot } from 'react-dom/client'
-import App from './components/App.js'
+import App from './components/App'
+import { ApolloProvider } from '@apollo/client'
+import client from './graphql/client'
 import './styles/index.css'
 
-createRoot(document.getElementById('root')!).render(
-  <StrictMode>
+const root = createRoot(document.getElementById('root')!);
+root.render(
+  <ApolloProvider client={client}>
     <App />
-  </StrictMode>,
-)
+  </ApolloProvider>
+);
