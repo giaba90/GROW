cb43405fe4f8d0a961d182137de4d9cbfd454d5b
Gianluca <giaba90@gmail.com>
2024-10-19 10:31:30 +0200

    create styles dir

--- client/src/components/App.tsx
+++ client/src/components/App.tsx
@@ -1,8 +1,8 @@
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import { ApolloClient, InMemoryCache, ApolloProvider } from '@apollo/client';
-import Homepage from './pages/homepage';
-import Archive from './pages/archive';
-import SinglePost from './pages/single-post';
+import Homepage from '../pages/homepage';
+import Archive from '../pages/archive';
+import SinglePost from '../pages/single-post';
 
 const client = new ApolloClient({
   uri: 'http://localhost:8000/graphql', // Sostituisci con l'URL del tuo endpoint GraphQL di WordPress
