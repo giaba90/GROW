ff1a5b351cf059c9007546c098fb180809f693ea
Gianluca <giaba90@gmail.com>
2024-10-22 18:57:21 +0200

    add my custom NavBar comp

---
+++ client/src/components/ui/Navbar.tsx
@@ -0,0 +1,29 @@
+import React from 'react';
+
+const Navbar: React.FC<{ items: { id: string; uri: string; label: string }[] }> = ({ items }) => {
+    return (
+        <nav className="p-4">
+            <div className="container mx-auto flex justify-between items-center">
+                {/* Menu Items */}
+                <ul className="flex space-x-4">
+                    {items.map((item) => (
+                        <NavBarItem key={item.id} url={item.uri} label={item.label}>
+                        </NavBarItem>
+                    ))}
+                </ul>
+            </div>
+        </nav>
+    );
+}
+
+const NavBarItem: React.FC<{ url: string; label: string }> = ({ url, label }) => {
+    return (
+        <li>
+            <a href={url} className="text-gray-300 hover:text-white px-3 py-2 rounded-md text-sm font-medium">
+                {label}
+            </a>
+        </li>
+    );
+}
+
+export default Navbar;
