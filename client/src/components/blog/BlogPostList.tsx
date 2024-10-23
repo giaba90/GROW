9256ee4219387c6b9717313c2d0c201d2fc4d651
Gianluca <giaba90@gmail.com>
2024-10-23 18:59:38 +0200

    refactory code

---
+++ client/src/components/blog/BlogPostList.tsx
@@ -0,0 +1,19 @@
+import BlogPostCard from '@/components/blog/BlogPostCard';
+import { Post } from '@/graphql/queries';
+
+// Correzione della tipizzazione: posts è direttamente un array di Post
+const BlogPostList: React.FC<{ posts: Post[] }> = ({ posts }) => {
+    return (
+        <div className="grid gap-8 md:grid-cols-2 lg:grid-cols-3">
+            {posts.length > 0 ? (
+                posts.map((post) => (
+                    <BlogPostCard key={post.postId} post={post} />
+                ))
+            ) : (
+                <p>No posts available.</p> // Fallback message if there are no posts
+            )}
+        </div>
+    );
+}
+
+export default BlogPostList;
