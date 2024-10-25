2cce586b9398940f1280ebf2619406cd1c49028d
Gianluca <giaba90@gmail.com>
2024-10-25 20:27:51 +0200

    wip: layout single post

--- client/src/pages/single-post.tsx
+++ client/src/pages/single-post.tsx
@@ -54,33 +54,28 @@ export default function SinglePost() {
     <div className="min-h-screen bg-background">
       <Header />
       <main className="container mx-auto md:p-4">
-        { /* 
-        <PostBreadcrumb title={post.title} /> 
-    Use the new component later*/}
-        <Card>
-          <CardHeader className="p-0">
-            <img src={post.featuredImage?.node.sourceUrl || "/placeholder.svg?height=400&width=800"} alt={post.title} className="w-full h-64 object-cover md:rounded-t-lg" />
-          </CardHeader>
-          <CardContent className="p-6">
-            <CardTitle className="text-3xl mb-4">{post.title}</CardTitle>
-            <div className="flex flex-wrap gap-2 mb-4 justify-between">
-              {post.tags.nodes.map((tag: { name: string }) => (
-                <Badge key={tag.name} variant="outline">#{tag.name}</Badge>
-              ))}
-              <div className="flex space-x-4">
-                <Share2 className="h-4 w-4 mr-2" />
-                <Bookmark className="h-4 w-4 mr-2" />
-                <ThumbsUp className="h-4 w-4 mr-2" />
-              </div>
-            </div>
-            <div className="prose max-w-none mb-4" dangerouslySetInnerHTML={{ __html: post.content }} />
-            <div className="flex items-center space-x-4 text-muted-foreground mb-4">
-              <span>Posted on {new Date(post.date).toLocaleDateString()} by <b>{post.author.node.name}</b></span>
-            </div>
-          </CardContent>
-        </Card>
-      </main>
+        <PostBreadcrumb title={post.title} />
+        <img src={post.featuredImage?.node.sourceUrl || "/placeholder.svg?height=400&width=800"} alt={post.title} className="w-full h-64 object-cover" />
+        <h2 className='text-base md:text-2xl my-4'> {post.title} </h2>
+        <div className="flex flex-wrap gap-2 justify-between">
+          <div className="flex items-center space-x-4 text-muted-foreground mb-4">
+            <span>Posted on {new Date(post.date).toLocaleDateString()} by <b>{post.author.node.name}</b></span>
+          </div>
+          <div className="flex space-x-4">
+            <Share2 className="h-4 w-4 mr-2" />
+            <Bookmark className="h-4 w-4 mr-2" />
+            <ThumbsUp className="h-4 w-4 mr-2" />
+          </div>
+        </div>
+        <hr></hr>
+        <div className="prose max-w-none mt-4 mb-4" dangerouslySetInnerHTML={{ __html: post.content }} />
 
+        <span className="block mt-4 mb-4">
+          {post.tags.nodes.map((tag: { name: string }) => (
+            <Badge key={tag.name} variant="outline">#{tag.name}</Badge>
+          ))}
+        </span>
+      </main>
       <Footer></Footer>
     </div>
   )
