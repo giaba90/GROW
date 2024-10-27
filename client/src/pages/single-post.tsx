674d118be4b3f04239b31b2b97b0c4ab1f9ffbc6
Gianluca <giaba90@gmail.com>
2024-10-27 10:21:11 +0100

    change icons and colors

--- client/src/pages/single-post.tsx
+++ client/src/pages/single-post.tsx
@@ -1,43 +1,12 @@
-import { useParams, Link } from 'react-router-dom'
-import { useQuery, gql } from '@apollo/client'
-import { Bookmark, Share2, ThumbsUp } from 'lucide-react'
-import { Card, CardContent, CardHeader, CardTitle } from "@/components/ui/card"
+import { useParams } from 'react-router-dom'
+import { useQuery } from '@apollo/client'
+import { Share2, MessageCircle, Heart } from 'lucide-react'
 import { Badge } from "@/components/ui/badge"
 import Footer from '@/components/layout/Footer'
 import Header from '@/components/layout/Header'
 import PostBreadcrumb from '@/components/blog/PostBreadcrumb';
+import { GET_POST } from '@/graphql/queries'
 
-const GET_POST = gql`
-  query GetPost($id: ID!) {
-    post(id: $id, idType: DATABASE_ID) {
-      postId
-      title
-      date
-      content
-      featuredImage {
-        node {
-          sourceUrl
-        }
-      }
-      author {
-        node {
-          name
-        }
-      }
-      categories {
-        nodes {
-          name
-          slug
-        }
-      }
-      tags {
-        nodes {
-          name
-        }
-      }
-    }
-  }
-`;
 
 export default function SinglePost() {
   const { id } = useParams();
@@ -55,19 +24,27 @@ export default function SinglePost() {
       <Header />
       <main className="container mx-auto md:p-4">
         <PostBreadcrumb title={post.title} />
-        <img src={post.featuredImage?.node.sourceUrl || "/placeholder.svg?height=400&width=800"} alt={post.title} className="w-full h-64 object-cover" />
         <h2 className='text-base md:text-2xl my-4'> {post.title} </h2>
         <div className="flex flex-wrap gap-2 justify-between">
           <div className="flex items-center space-x-4 text-muted-foreground mb-4">
-            <span>Posted on <strong>Category</strong> ,{new Date(post.date).toLocaleDateString()} by <b>{post.author.node.name}</b></span>
+            <span>Posted on {post.categories.nodes.map((cat: { name: string }) => (<strong>{cat.name}</strong>))} , {new Date(post.date).toLocaleDateString('it-IT', { day: 'numeric', month: 'long', year: 'numeric' })} by <b>{post.author.node.name}</b></span>
           </div>
+
           <div className="flex space-x-4">
-            <Share2 className="h-4 w-4 mr-2" />
-            <Bookmark className="h-4 w-4 mr-2" />
-            <ThumbsUp className="h-4 w-4 mr-2" />
+            <div className="hover:text-blue-500">
+              <MessageCircle className="h-4 w-4 mr-2" />
+            </div>
+            <div className="hover:text-red-500">
+              <Heart className="h-4 w-4 mr-2" />
+            </div>
+            <div className="hover:text-green-500">
+              <Share2 className="h-4 w-4 mr-2" />
+            </div>
           </div>
         </div>
-        <hr></hr>
+        <img src={post.featuredImage?.node.sourceUrl || "/placeholder.svg?height=400&width=800"} alt={post.title} className="w-full h-64 object-cover" />
+
+
         <div className="prose max-w-none mt-4 mb-4" dangerouslySetInnerHTML={{ __html: post.content }} />
 
         <span className="block mt-4 mb-4">
