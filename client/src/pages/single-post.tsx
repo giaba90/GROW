1f724bc36323da8d5dc56ebd58a1625fe2423064
Gianluca <giaba90@gmail.com>
2024-10-28 19:12:13 +0100

    add back button

--- client/src/pages/single-post.tsx
+++ client/src/pages/single-post.tsx
@@ -1,9 +1,12 @@
-import { useParams } from 'react-router-dom';
+import { useParams} from 'react-router-dom';
 import { useQuery } from '@apollo/client';
 import Footer from '@/components/layout/Footer';
 import Header from '@/components/layout/Header';
-import { GET_POST } from '@/graphql/queries';
 import PostDetails from '@/components/blog/PostDetails';
+import { Button } from '@/components/ui/button';
+import { MoveLeft } from 'lucide-react';
+import { GET_POST } from '@/graphql/queries';
+
 
 const Loading = () => <p>Loading...</p>;
 const ErrorMessage = ({ message }: { message: string }) => <p>Error: {message}</p>;
@@ -20,8 +23,12 @@ const SinglePost: React.FC = () => {
   return (
     <div className="min-h-screen bg-background">
       <Header />
-      <main className="container mx-auto md:p-4">
+      <main className="container mx-auto p-4">
+        <Button size="sm" variant="outline" onClick={() => window.history.back()} >
+          <MoveLeft />  Torna indietro
+        </Button>
         <PostDetails post={data.post} />
+        <h3 className="text-xl md:text-2xl my-4">Potrebbe interessarti anche</h3>
       </main>
       <Footer />
     </div>
