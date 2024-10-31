a9735c1ae3b7b58ae4ac49d4f1d2aeed1123332b
Gianluca <giaba90@gmail.com>
2024-10-31 22:01:38 +0100

    fix uri graphql ssl

--- client/src/graphql/client.ts
+++ client/src/graphql/client.ts
@@ -1,7 +1,7 @@
 import { ApolloClient, InMemoryCache } from '@apollo/client';
 
 const client = new ApolloClient({
-    uri: 'http://51.21.6.145/graphql', // Sostituisci con l'URL del tuo endpoint GraphQL di WordPress
+    uri: 'https://51.21.6.145/graphql', // Sostituisci con l'URL del tuo endpoint GraphQL di WordPress
     cache: new InMemoryCache(),
 });
 
