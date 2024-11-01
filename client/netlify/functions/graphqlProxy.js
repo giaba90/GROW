382c95e199899ffc238e42eb443225287552f9c6
Gianluca <giaba90@gmail.com>
2024-11-01 11:13:39 +0100

    Configura Netlify Functions

---
+++ client/netlify/functions/graphqlProxy.js
@@ -0,0 +1,28 @@
+const fetch = require('node-fetch');
+
+exports.handler = async function (event, context) {
+    // URL del server di destinazione
+    const url = 'https://51.21.6.145/graphql';
+
+    // Inoltra la richiesta al server
+    const response = await fetch(url, {
+        method: 'POST',
+        headers: {
+            'Content-Type': 'application/json',
+            'Authorization': event.headers.authorization || '', // se necessario
+        },
+        body: event.body, // inoltra il body della richiesta originale
+    });
+
+    // Ricevi la risposta e restituiscila al client
+    const data = await response.json();
+
+    return {
+        statusCode: response.status,
+        body: JSON.stringify(data),
+        headers: {
+            'Access-Control-Allow-Origin': '*', // Puoi specificare l'origine in modo più restrittivo, se necessario
+            'Access-Control-Allow-Headers': 'Content-Type, Authorization',
+        },
+    };
+};
--- client/src/graphql/client.ts
+++ client/src/graphql/client.ts
@@ -1,7 +1,7 @@
 import { ApolloClient, InMemoryCache } from '@apollo/client';
 
 const client = new ApolloClient({
-    uri: 'https://51.21.6.145/graphql', // Sostituisci con l'URL del tuo endpoint GraphQL di WordPress
+    uri: '/graphql', // Sostituisci con l'URL del tuo endpoint GraphQL di WordPress
     cache: new InMemoryCache(),
 });
 
