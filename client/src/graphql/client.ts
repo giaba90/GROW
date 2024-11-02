82dbed0d5cdc94ff0a57887c920dec5a2cf8af23
Gianluca <giaba90@gmail.com>
2024-11-02 22:09:45 +0100

    connectToDevTools option in your ApolloClient instance

--- client/src/graphql/client.ts
+++ client/src/graphql/client.ts
@@ -3,6 +3,15 @@ import { ApolloClient, InMemoryCache } from '@apollo/client';
 const client = new ApolloClient({
     uri: 'https://51.21.6.145/wordpress/graphql', // Sostituisci con l'URL del tuo endpoint GraphQL di WordPress
     cache: new InMemoryCache(),
+    // Provide some optional constructor fields
+    name: 'react-web-client',
+    version: '1.3',
+    queryDeduplication: false,
+    defaultOptions: {
+        watchQuery: {
+            fetchPolicy: 'cache-and-network',
+        },
+    },
 });
 
 export default client;
