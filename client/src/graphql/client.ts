be14c48266a8580c553be24f9b985cb9ae793145
Gianluca <giaba90@gmail.com>
2024-11-02 22:20:37 +0100

    configurazione lato client come workaround

--- client/src/graphql/client.ts
+++ client/src/graphql/client.ts
@@ -1,17 +1,25 @@
-import { ApolloClient, InMemoryCache } from '@apollo/client';
+import { ApolloClient, InMemoryCache, HttpLink, ApolloLink } from '@apollo/client';
 
-const client = new ApolloClient({
-    uri: 'https://51.21.6.145/wordpress/graphql', // Sostituisci con l'URL del tuo endpoint GraphQL di WordPress
-    cache: new InMemoryCache(),
-    // Provide some optional constructor fields
-    name: 'react-web-client',
-    version: '1.3',
-    queryDeduplication: false,
-    defaultOptions: {
-        watchQuery: {
-            fetchPolicy: 'cache-and-network',
+// Definisci il link HTTP per il tuo server GraphQL
+const httpLink = new HttpLink({
+    uri: 'https://51.21.6.145/wordpress/graphql',
+});
+
+// Crea un link che rimuove l'intestazione 'apollographql-client-version'
+const removeHeaderLink = new ApolloLink((operation, forward) => {
+    operation.setContext(({ headers = {} }) => ({
+        headers: {
+            ...headers,
+            'apollographql-client-version': undefined, // Rimuove l'intestazione specifica
         },
-    },
+    }));
+    return forward(operation);
+});
+
+// Configura Apollo Client con il link modificato
+const client = new ApolloClient({
+    link: ApolloLink.from([removeHeaderLink, httpLink]), // Combina i link
+    cache: new InMemoryCache(),
 });
 
 export default client;
