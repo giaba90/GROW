d96295983022bec8ee1a8622e1969d1b431158ef
Gianluca <giaba90@gmail.com>
2024-11-04 21:17:55 +0100

    add shadow card

--- client/src/components/ui/card.tsx
+++ client/src/components/ui/card.tsx
@@ -9,7 +9,7 @@ const Card = React.forwardRef<
   <div
     ref={ref}
     className={cn(
-      "bg-card text-card-foreground md:shadow",
+      "bg-card text-card-foreground shadow",
       className
     )}
     {...props}
