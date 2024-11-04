a52503693599ae1da693e71ee1a73e115228e48e
Gianluca <giaba90@gmail.com>
2024-11-04 20:05:13 +0100

    delete js files

--- client/src/components/layout/Footer.js
+++
@@ -1,6 +0,0 @@
-import { jsx as _jsx, jsxs as _jsxs } from "react/jsx-runtime";
-import { Link } from 'react-router-dom';
-import { Button } from "@/components/ui/button";
-export default function Footer() {
-    return (_jsx("footer", { className: "bg-secondary text-secondary-foreground p-4 mt-8", children: _jsxs("div", { className: "container mx-auto text-center", children: [_jsx("p", { children: "\u00A9 2024 Grow. Tutti i diritti riservati." }), _jsx(Button, { variant: "link", asChild: true, children: _jsx(Link, { to: "/privacy-policy", children: "Privacy Policy" }) })] }) }));
-}
--- client/src/components/ui/Navbar.js
+++
@@ -1,8 +0,0 @@
-import { jsx as _jsx } from "react/jsx-runtime";
-const Navbar = ({ items }) => {
-    return (_jsx("nav", { className: "p-4", children: _jsx("div", { className: "container mx-auto flex justify-between items-center", children: _jsx("ul", { className: "flex space-x-4", children: items.map((item) => (_jsx(NavBarItem, { url: item.uri, label: item.label }, item.id))) }) }) }));
-};
-const NavBarItem = ({ url, label }) => {
-    return (_jsx("li", { children: _jsx("a", { href: url, className: "text-gray-300 hover:text-white px-3 py-2 rounded-md text-sm font-medium", children: label }) }));
-};
-export default Navbar;
