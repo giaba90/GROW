89e2f86642d100818673972df32c8e0a16190f39
Gianluca <giaba90@gmail.com>
2024-11-08 13:15:39 +0100

    feat: add LikeButton component and integrate it into PostDetails for user interaction

--- client/src/components/blog/PostDetails.tsx
+++ client/src/components/blog/PostDetails.tsx
@@ -2,6 +2,8 @@ import { Post } from '@/types/post';
 import { PostMeta } from './PostMeta';
 import { PostTags } from './PostTags';
 import SocialIcons from './SocialIcons';
+import { Heart, MessageCircle } from 'lucide-react';
+import LikeButton from './LikeButton';
 
 type PostDetailsProps = {
     post: Post;
@@ -12,10 +14,14 @@ export default function PostDetails({ post }: PostDetailsProps) {
 
     return (
         <>
+            <SocialIcons pageTitle={title} url={`https://wpgrow.netlify.app/post/${postId}`} />
             <h2 className="text-2xl md:text-4xl my-4">{title}</h2>
             <div className="flex flex-wrap gap-2 justify-between">
                 <PostMeta categories={categories} date={date} author={author} />
-                <SocialIcons pageTitle={title} url={`https://wpgrow.netlify.app/post/${postId}`} />
+                <div className="flex flex-row items-baseline">
+                    <MessageCircle className="h-4 w-4 mr-2 hover:text-blue-500" />
+                    <LikeButton initialLikes={0} />
+                </div>
             </div>
             <img
                 src={featuredImage?.node.sourceUrl || '/placeholder.svg?height=400&width=800'}
