e294b3a1a479d336f3d6241c2b39dfd23fa07144
Gianluca Barranca <info@gianlucabarranca.it>
2024-11-16 11:33:09 +0100

    translate text

--- client/src/components/blog/PostMeta.tsx
+++ client/src/components/blog/PostMeta.tsx
@@ -6,13 +6,12 @@ type PostMetaProps = {
     author: { node: { name: string } };
 };
 
-
 export const PostMeta = ({ categories, date, author }: PostMetaProps) => (
     <div className="flex items-center space-x-4 text-muted-foreground mb-4">
         <span className='text-sm md:text-base'>
-            Posted on {categories.nodes.map((cat) => (
+            Pubblicato in {categories.nodes.map((cat) => (
                 <strong key={cat.name}>{cat.name}</strong>
-            ))}, {formatDate(date)} by <b>{author.node.name}</b>
+            ))}, il {formatDate(date)} da <b>{author.node.name}</b>
         </span>
     </div>
 );
